fb5b1deb0243066d89ca684136fed096733ccba8
flyingking888 <jeremyfeiwang@outlook.com>
2026-03-02 16:01:36 -0500

    3-2-2026

--- FlaglerSwiftUI/FlaglerSwiftUIApp.swift
+++ FlaglerSwiftUI/FlaglerSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FlaglerSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProfileView()
         }
     }
 }
--- FlaglerSwiftUI/ProfileView.swift
+++ FlaglerSwiftUI/ProfileView.swift
@@ -6,18 +6,35 @@
 //
 
 import SwiftUI
+// Define a new structure named ProfileView.
+// In SwiftUI, views are typically defined as structs (value types).
+
+// Conforms to the View protocol (similar to inheritance, but different!)
+
+// View represents any UI element in SwifUI. They don't start with dot!
+// Modifiers are methods that change a view. They start with dot!!!
 
 struct ProfileView: View {
+    // The body property is required by the View protocol. It describes the UI layout for this view.
     var body: some View {
+        //Zack is a layered view container
         ZStack{
+            //Color is actually a view here.
             Color(UIColor(red: 0.75, green: 0.22, blue: 0.17, alpha: 1))
                 .ignoresSafeArea()
-            
+                        
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
+                //ImageView
+                Image("Jeremy")
+                    //These are modifiers of Image! :)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 10))
+                //TextView
                 Text("Hello, world! My name is Jeremy")
+                    //These are modifers of Text.
                     .font(Font.title3.bold())
                     .italic()
             }
