9c6fcb9faced988f9628044d979b613b405d73fa
flyingking888 <jeremyfeiwang@outlook.com>
2026-03-02 14:36:37 -0500

    Initial Commit

---
+++ FlaglerSwiftUI/FlaglerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaglerSwiftUIApp.swift
+//  FlaglerSwiftUI
+//
+//  Created by Jeremy Wang on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct FlaglerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
