3559e5c9706d71fee8d33a49f9fe641bb3a2721b
flyingking888 <jeremyfeiwang@outlook.com>
2026-04-01 21:39:41 -0400

    4-1-2026

---
+++ FlaglerSwiftUI/View/TipView.swift
@@ -0,0 +1,18 @@
+//
+//  TipView.swift
+//  FlaglerSwiftUI
+//
+//  Created by Jeremy Wang on 4/1/26.
+//
+
+import SwiftUI
+
+struct TipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipView()
+}
